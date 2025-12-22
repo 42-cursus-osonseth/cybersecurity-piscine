113b47ae687fae27dcc27c18c4aba7d412e08f18
Osonseth <max.cesaria@gmail.com>
2025-12-22 18:49:36 +0100

    add: VALID EXT in const.hpp

--- Stockholm/include/const.hpp
+++ Stockholm/include/const.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 inline const std::string VERSION = "Version: 1.0";
 inline const std::string HELP = R"(
@@ -12,4 +13,49 @@ Options:
   -s, --silent          Suppress all output
   -r, --reverse KEY     Reverse the infection using the given KEY (must be at least 16 characters)
 )";
+
+inline const std::vector<std::string> VALID_EXT = {
+    ".123",
+    ".3dm", ".3ds", ".3g2", ".3gp",
+    ".602",
+    ".ARC", ".PAQ",
+    ".accdb", ".aes", ".asc", ".asm", ".asp", ".avi",
+    ".bak", ".backup", ".bat", ".bmp", ".brd", ".bz2",
+    ".cgm", ".class", ".cmd", ".cpp", ".crt", ".csr", ".csv",
+    ".dbf", ".der", ".dif", ".dip", ".djvu", ".dch",
+    ".doc", ".docb", ".docm", ".docx", ".dot", ".dotm", ".dotx",
+    ".dwg",
+    ".edb", ".eml",
+    ".fla", ".flv", ".frm",
+    ".gif", ".gpg",
+    ".hwp",
+    ".ibd", ".iso",
+    ".jar", ".java", ".jpeg", ".jpg", ".jsp",
+    ".key",
+    ".lay", ".lay6", ".ldf",
+    ".max", ".mdb", ".mdf", ".mid", ".mkv", ".mml",
+    ".mov", ".mp3", ".mp4", ".mpeg", ".mpg", ".msg",
+    ".myd", ".myi",
+    ".nef",
+    ".odb", ".odg", ".odp", ".ods", ".odt", ".onetoc2",
+    ".otg", ".otp", ".ots", ".ott",
+    ".pas", ".pdf", ".pem", ".pfx", ".php",
+    ".png", ".pot", ".potm", ".potx",
+    ".ppam", ".pps", ".ppsm", ".ppsx",
+    ".ppt", ".pptm", ".pptx",
+    ".ps1", ".psd", ".pst",
+    ".raw", ".rar", ".rtf",
+    ".sch", ".sldm", ".sldx", ".slk", ".sln",
+    ".sql", ".sqlite3", ".sqlitedb",
+    ".std", ".sti", ".stw", ".suo", ".svg",
+    ".sxc", ".sxd", ".sxi", ".sxm", ".snt",
+    ".tar", ".tbk", ".tgz", ".tif", ".tiff", ".txt",
+    ".uot",
+    ".vbs", ".vcd", ".vdi", ".vmx", ".vmdk",
+    ".vsd", ".vsdx", ".vob",
+    ".wav", ".wb2", ".wks", ".wk1", ".wmv",
+    ".xls", ".xlsb", ".xlsm", ".xlsx",
+    ".xlc", ".xlm", ".xlt", ".xltm", ".xltx", ".xlw",
+    ".zip"
+};
 
